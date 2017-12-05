0e2c9cbea9445ad1ec6c75ad6710bcf0fe37debf
Maurice Roy <mautayro@gmail.com>
2017-12-05 19:18:16 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,11 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |category, category_data|
     category_data.each do |attribute, pigeons| #creates containers
       pigeons.each do |pigeon|
-        if pigeon_list.keys.include?(pigeon)
-          #bad code here?
-        else
-          pigeon_list[pigeon] = {}
-        end
+        pigeon_list[pigeon] = {}
       end
     end
     pigeon_list.each do |pigeon, pigeon_data| #inserts data
